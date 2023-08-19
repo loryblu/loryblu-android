792c6a7c5d6fb599002e15d4c17f87185834387b
OdisBy <ruliam@proton.me>
2023-08-19 11:59:38 -0300

    Push

--- app/src/main/java/com/example/loryblu/forgotpassword/ForgotPasswordViewModel.kt
+++ app/src/main/java/com/example/loryblu/forgotpassword/ForgotPasswordViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.loryblu.forgotpassword
 
-import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
