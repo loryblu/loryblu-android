0a68c3c969adc5301ad8dc1f3fe52dddafb8834c
André Moraes Filho <softdev.andre@gmail.com>
2023-09-07 19:15:39 -0300

    setted verticalScroll

--- app/src/main/java/com/example/loryblu/login/LoginScreen.kt
+++ app/src/main/java/com/example/loryblu/login/LoginScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
@@ -55,7 +57,7 @@ fun LoginScreen(
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.padding(P_SMALL)
+        modifier = Modifier.padding(P_SMALL).verticalScroll(rememberScrollState())
     ) {
         LBTitle(textRes = R.string.login_title)
 
