d496bd534b7906beff77b35a12e4af83d72be7ac
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2023-10-02 08:13:21 -0300

    :arrow_up: build: Changed dependency name to correct one

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.compose.bom))
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
