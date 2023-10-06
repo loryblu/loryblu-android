a67b4c611f9e2b67d8e381f012b7f7b352a7284a
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2023-10-06 08:49:12 -0300

    :sparkles: feat: Added Ktor
    Co-authored-by: Jean Patrick Hartmann <32403781+hartmannjean@users.noreply.github.com>

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,54 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    kotlin("plugin.serialization") version "1.9.0"
+}
+
+android {
+    namespace = "com.loryblu.core.network"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.koin.androidx.compose)
+
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.serialization)
+    implementation(libs.ktor.gson)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ core/network/src/main/java/com/loryblu/core/network/di/networkModule.kt
@@ -0,0 +1,24 @@
+package com.loryblu.core.network.di
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.gson.gson
+import org.koin.dsl.module
+
+val networkModule = module {
+    single<HttpClient> {
+        HttpClient(Android){
+            install(Logging){
+                level = LogLevel.ALL
+            }
+            install(ContentNegotiation){
+                gson {
+                    serializeNulls()
+                }
+            }
+        }
+    }
+}
