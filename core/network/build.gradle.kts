f4e80427df0457b64ea961dfa5a7c948e9cfb6a8
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2023-10-06 08:49:12 -0300

    :sparkles: feat: Added Ktor
    Co-authored-by: Jean Patrick Hartmann <32403781+hartmannjean@users.noreply.github.com>

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -36,10 +36,20 @@ android {
 
 dependencies {
     implementation(libs.koin.androidx.compose)
+
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.koin.androidx.compose)
+
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.android)
     implementation(libs.ktor.client.logging)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.serialization)
     implementation(libs.ktor.gson)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
