9e93542819c098882d460e7e045bfecbfa37a794
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2023-10-06 17:15:42 -0300

    :bug: fix: Added HttpRoutes

--- data/auth/src/main/java/com/loryblu/data/auth/HttpRoutes.kt
+++ data/auth/src/main/java/com/loryblu/data/auth/HttpRoutes.kt
@@ -4,5 +4,4 @@ object HttpRoutes {
     private const val BASE_URL = BuildConfig.BASE_URL
     const val REGISTER = "$BASE_URL/auth/register"
     const val RECOVERY_PASSWORD = "$BASE_URL/auth/recovery"
-
 }
