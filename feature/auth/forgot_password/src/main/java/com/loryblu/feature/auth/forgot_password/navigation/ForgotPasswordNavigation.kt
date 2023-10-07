24992229657367cfc08c3eaf137f39913403d4b6
Jean Patrick Hartmann <32403781+hartmannjean@users.noreply.github.com>
2023-10-06 22:18:06 -0300

    :sparkles: added forgot password api request

--- feature/auth/forgot_password/src/main/java/com/loryblu/feature/auth/forgot_password/navigation/ForgotPasswordNavigation.kt
+++ feature/auth/forgot_password/src/main/java/com/loryblu/feature/auth/forgot_password/navigation/ForgotPasswordNavigation.kt
@@ -1,13 +1,9 @@
 package com.loryblu.feature.auth.forgot_password.navigation
 
-import android.util.Log
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.loryblu.core.util.Screen
-import androidx.compose.runtime.getValue
-import androidx.navigation.navDeepLink
-import com.loryblu.feature.auth.create_password.navigation.DeepLinkPattern
 import com.loryblu.feature.auth.forgot_password.ForgotPasswordScreen
 import com.loryblu.feature.auth.forgot_password.ForgotPasswordViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -16,7 +12,7 @@ fun NavGraphBuilder.forgotPasswordRoute(
     navigateToCreatePassword: () -> Unit,
 ) {
     composable(
-        route = Screen.ForgetPassword.route,
+        route = Screen.ForgetPassword.route
     ) {
         val viewModel: ForgotPasswordViewModel = koinViewModel()
         val authenticated by viewModel.authenticated
