696a177574944be4b33df158697b31100c01d480
Jean Patrick Hartmann <32403781+hartmannjean@users.noreply.github.com>
2023-10-07 10:25:38 -0300

    construction: :sparkles: added deep link

--- core/util/src/main/java/com/loryblu/core/util/Screen.kt
+++ core/util/src/main/java/com/loryblu/core/util/Screen.kt
@@ -4,7 +4,7 @@ sealed class Screen(val route: String) {
     object Login: Screen(route = "login_screen")
     object RegisterGuardian: Screen(route = "register_guardian_screen")
     object RegisterChild: Screen(route = "register_child_screen")
-    object CreatePassword: Screen(route = "create_password_screen/{token}")
+    object CreatePassword: Screen(route = "password_recovery/{token}")
     object ForgetPassword: Screen(route = "forget_password_screen")
     object Home: Screen(route = "home_screen")
     object RegistrationConfirmed: Screen(route = "registration_confirmed_screen")
--- feature/auth/create_password/src/main/java/com/loryblu/feature/auth/create_password/navigation/CreatePasswordNavigation.kt
+++ feature/auth/create_password/src/main/java/com/loryblu/feature/auth/create_password/navigation/CreatePasswordNavigation.kt
@@ -1,10 +1,12 @@
 package com.loryblu.feature.auth.create_password.navigation
 
+import android.util.Log
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.loryblu.core.util.Screen
 import androidx.compose.runtime.getValue
+import androidx.navigation.navDeepLink
 import com.loryblu.feature.auth.create_password.CreatePasswordScreen
 import com.loryblu.feature.auth.create_password.CreatePasswordViewModel
 
@@ -12,7 +14,17 @@ import com.loryblu.feature.auth.create_password.CreatePasswordViewModel
 fun NavGraphBuilder.createPasswordRoute(
     navigateToLoginScreen: () -> Unit,
 ) {
-    composable(route = Screen.CreatePassword.route) {
+
+    composable(
+        route = Screen.CreatePassword.route,
+        deepLinks = listOf(
+            navDeepLink {
+                uriPattern = "loryblu://password_recovery/{token}"
+            }
+        ),
+    ) { backStack ->
+        val token = backStack.arguments?.getString("token")
+        Log.d("token", "createPasswordRoute: $token")
         val viewModel: CreatePasswordViewModel = viewModel()
         val shouldGoToNextScreen by viewModel.shouldGoToNextScreen
         CreatePasswordScreen(
