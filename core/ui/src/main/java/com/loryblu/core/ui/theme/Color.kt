8cba90b570a76caa2ff9ae53cc969f1cf9f656ab
André Moraes Filho <softdev.andre@gmail.com>
2023-10-15 22:49:30 -0300

    :sparkles: feat: Added colors to follow app Figma design system

--- core/ui/src/main/java/com/loryblu/core/ui/theme/Color.kt
+++ core/ui/src/main/java/com/loryblu/core/ui/theme/Color.kt
@@ -2,19 +2,14 @@ package com.loryblu.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-// colors of the project
-val Blue = Color(0xff2491d0)
-val LightGray = Color(0xffe8eafd)
-val Black = Color(0xff0f1326)
-val Gray = Color(0xff9b9797)
-val White = Color(0xfffdfdfd)
-val Error = Color(0xffd62c2c)
-val DarkBlue = Color(0xff004a98)
+// Project Colors
+val LBSoftBlue = Color(0xffE8EAFD)
+val LBSkyBlue = Color(0xff2491D0)
+val LBLightGray = Color(0xffC4C4C4)
+val LBDarkBlue = Color(0xff004A98)
+val LBSoftGray = Color(0xffFDFDFD)
+val LBSilverGray = Color(0xff9B9797)
+val LBShadowGray = Color(0xff0F1326)
+val LBMediumGray = Color(0xff616161)
+val LBSmokeGray = Color(0xff49454F)
+val LBErrorColor = Color(0xffD62C2C)
