2ef61b8945079c45a5101e5c8046245d10ccfd49
André Moraes Filho <softdev.andre@gmail.com>
2023-10-15 22:51:47 -0300

    :sparkles: feat: Added date format to date picker headline and refactored the other date formats functions

--- core/util/src/main/java/com/loryblu/core/util/extensions/LBDateFormats.kt
+++ core/util/src/main/java/com/loryblu/core/util/extensions/LBDateFormats.kt
@@ -7,19 +7,61 @@ import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
 
-fun Long.toBrazilianDateFormat(): String {
+fun Long.toDateFormat(): String {
+    val currentLocale = Locale.getDefault()
+
+    val languagePatterns = mapOf(
+        "pt" to "dd/MM/yyyy",
+        "en" to "MM/dd/yyyy"
+    )
+
+    val pattern = languagePatterns[currentLocale.language] ?: "dd/MM/yyyy"
+
     val date = Date(this)
     val formatter = SimpleDateFormat(
-        "dd/MM/yyyy", Locale("pt-br")
+        pattern, currentLocale
     ).apply {
         timeZone = TimeZone.getTimeZone("GMT")
     }
     return formatter.format(date)
 }
 
+fun Long.toHeadLineDateFormat(): String {
+    val currentLocale = Locale.getDefault()
+
+    val languagePatterns = mapOf(
+        "pt" to "E, MMM d",
+        "en" to "E, MMM d"
+    )
+
+    val pattern = languagePatterns[currentLocale.language] ?: "E, MMM d"
+
+    val date = Date(this)
+    val formatter = SimpleDateFormat(pattern, currentLocale).apply {
+        timeZone = TimeZone.getTimeZone("GMT")
+    }
+
+    val formattedString = formatter.format(date).replace(".", "")
+
+    return formattedString.split(" ")
+        .joinToString(" ") {
+            it.replaceFirstChar { char ->
+                if (char.isLowerCase()) char.titlecase(currentLocale) else char.toString()
+            }
+        }
+}
+
 fun String.toApiFormat() : String {
-    val oldFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-    val date = LocalDate.parse(this, oldFormat)
+    val currentLocale = Locale.getDefault()
+    val oldFormat = mapOf(
+        "pt" to DateTimeFormatter.ofPattern("dd/MM/yyyy"),
+        "en" to DateTimeFormatter.ofPattern("MM/dd/yyyy")
+    )
+    val date = LocalDate.parse(
+        this,
+        oldFormat[currentLocale.language]
+            ?: DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    )
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
     return date.format(formatter)
 }
