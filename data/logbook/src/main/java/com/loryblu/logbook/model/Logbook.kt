b0c02482661c93f474183fb3f470d2150cbee039
Jean Patrick Hartmann <32403781+hartmannjean@users.noreply.github.com>
2023-10-23 22:56:20 -0300

    :sparkles: feat: Export and map images

---
+++ data/logbook/src/main/java/com/loryblu/logbook/model/Logbook.kt
@@ -0,0 +1,15 @@
+package com.loryblu.logbook.model
+
+import com.loryblu.core.ui.R
+
+data class Routine(
+    override val idImage: Int = 0,
+    override val imageText: Int = R.string.lory_routine,
+    override val imageDrawable: Int = R.drawable.lory_routine
+) : Item
+
+data class Student(
+    override val idImage: Int = 2,
+    override val imageText: Int = R.string.lory_student,
+    override val imageDrawable: Int = R.drawable.lory_student
+) : Item
