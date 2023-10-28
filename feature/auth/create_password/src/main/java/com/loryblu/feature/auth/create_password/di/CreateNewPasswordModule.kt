552cad8503173d449af4ebe13ca6bc919bfd1617
André Moraes Filho <softdev.andre@gmail.com>
2023-10-28 00:07:20 -0300

    :recycle: refactor: Refactored redundant Koin module declarations

--- feature/auth/create_password/src/main/java/com/loryblu/feature/auth/create_password/di/CreateNewPasswordModule.kt
+++ feature/auth/create_password/src/main/java/com/loryblu/feature/auth/create_password/di/CreateNewPasswordModule.kt
@@ -6,7 +6,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val createNewPasswordModule = module {
-    viewModel<CreatePasswordViewModel> {
+    viewModel {
         CreatePasswordViewModel(get())
     }
 }
