e94fcbf7244dc77e57c6e5dbfa5de87a5927a6b9
André Moraes Filho <softdev.andre@gmail.com>
2023-10-27 23:46:26 -0300

    :sparkles: feat: Added singleton dependency for LoginApi

--- data/auth/src/main/java/com/loryblu/data/auth/di/authModule.kt
+++ data/auth/src/main/java/com/loryblu/data/auth/di/authModule.kt
@@ -1,5 +1,7 @@
 package com.loryblu.data.auth.di
 
+import com.loryblu.data.auth.api.LoginApi
+import com.loryblu.data.auth.api.LoginApiImpl
 import com.loryblu.data.auth.api.NewPasswordApi
 import com.loryblu.data.auth.api.NewPasswordApiImpl
 import com.loryblu.data.auth.api.PasswordRecoveryApi
@@ -12,10 +14,16 @@ val authModule = module {
     single<RegisterApi> {
         RegisterApiImpl(get())
     }
+
     single<PasswordRecoveryApi> {
         PasswordRecoveryApiImpl(get())
     }
+
     single<NewPasswordApi> {
         NewPasswordApiImpl(get())
+    }
+
+    single<LoginApi> {
+        LoginApiImpl(get())
     }
 }
