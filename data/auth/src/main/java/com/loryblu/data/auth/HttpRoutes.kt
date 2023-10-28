91dc9723e9a8ed97fab488f10243a96244567876
André Moraes Filho <softdev.andre@gmail.com>
2023-10-28 00:49:06 -0300

    :sparkles: feat(HttpRoutes): added LOGIN route

--- data/auth/src/main/java/com/loryblu/data/auth/HttpRoutes.kt
+++ data/auth/src/main/java/com/loryblu/data/auth/HttpRoutes.kt
@@ -5,4 +5,5 @@ object HttpRoutes {
     const val REGISTER = "$BASE_URL/auth/register"
     const val RECOVERY_PASSWORD = "$BASE_URL/auth/recovery"
     const val NEW_PASSWORD = "$BASE_URL/auth/set-password"
+    const val LOGIN = "$BASE_URL/auth/login"
 }
