b2e7469bafe8a4cfeb9f59f402af110d0f98cb5a
André Moraes Filho <softdev.andre@gmail.com>
2023-10-28 01:19:31 -0300

    :sparkles: feat: Integrated Koin for Session and set start destination based on remember login status

--- app/src/main/java/com/loryblu/loryblu/MainActivity.kt
+++ app/src/main/java/com/loryblu/loryblu/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
+import com.loryblu.core.network.di.Session
 import com.loryblu.loryblu.navigation.SetupNavGraph
 import com.loryblu.core.ui.theme.LoryBluTheme
 import com.loryblu.core.util.Screen
+import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
+    private val session: Session by inject()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LoryBluTheme {
                 val navController = rememberNavController()
                 SetupNavGraph(
-                    startDestination = Screen.Login.route,
+                    startDestination = if(session.getRememberLogin()) {
+                        Screen.Home.route
+                    } else {
+                        Screen.Login.route
+                    },
                     navController = navController
                 )
             }
