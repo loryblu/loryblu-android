811fd21dc3114809dc5f59ec0da1a0d2ac13d8e6
André Moraes Filho <softdev.andre@gmail.com>
2023-10-28 01:27:12 -0300

    :sparkles: feat: added DataStore preferences configuration and refactored network module

--- core/network/src/main/java/com/loryblu/core/network/di/networkModule.kt
+++ core/network/src/main/java/com/loryblu/core/network/di/networkModule.kt
@@ -1,15 +1,20 @@
 package com.loryblu.core.network.di
 
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.gson.gson
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val networkModule = module {
-    single<HttpClient> {
+    single {
         HttpClient(Android){
             install(Logging){
                 level = LogLevel.ALL
@@ -21,5 +26,14 @@ val networkModule = module {
                 }
             }
         }
+    }
+
+    single {
+        PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(produceNewData = { emptyPreferences() }),
+            produceFile = { androidContext().preferencesDataStoreFile(Session.DATA) })
+    }
+    single {
+        Session(get())
     }
 }
