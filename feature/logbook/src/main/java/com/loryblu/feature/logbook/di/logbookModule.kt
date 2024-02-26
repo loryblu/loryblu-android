e8051cbecd8abce24fc0a711c7cae690c50e7a67
Rodrigo <rodrigocavalcante.pro@gmail.com>
2024-02-26 15:48:24 -0300

    [LOR-222] - Creating an instance of the LogbookHomeViewModel and its dependency on the LogbookModule.

--- feature/logbook/src/main/java/com/loryblu/feature/logbook/di/logbookModule.kt
+++ feature/logbook/src/main/java/com/loryblu/feature/logbook/di/logbookModule.kt
@@ -3,11 +3,16 @@ package com.loryblu.feature.logbook.di
 import com.loryblu.feature.logbook.ui.task.LogbookTaskViewModel
 import com.loryblu.feature.logbook.model.Category
 import com.loryblu.feature.logbook.model.LogbookTaskModel
+import com.loryblu.feature.logbook.ui.home.LogbookHomeViewModel
+import com.loryblu.feature.logbook.useCases.GetUserTaskByDayOfWeek
+import com.loryblu.feature.logbook.useCases.GetUserTaskByDayOfWeekImpl
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val logbookModule = module {
     viewModel { LogbookTaskViewModel(get(), get(), get()) }
+    viewModel { LogbookHomeViewModel(get())}
 
+    single<GetUserTaskByDayOfWeek> { GetUserTaskByDayOfWeekImpl(get()) }
     single<LogbookTaskModel> { LogbookTaskModel(Category.ROUTINE, "", "", listOf()) }
 }
