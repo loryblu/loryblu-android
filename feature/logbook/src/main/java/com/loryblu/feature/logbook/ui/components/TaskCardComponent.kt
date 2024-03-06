b915aeaddb0b232d85b03855a3d15c1e37a5a939
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2024-03-06 11:50:10 -0300

    :sparkles: feat(Layout): Task card component

---
+++ feature/logbook/src/main/java/com/loryblu/feature/logbook/ui/components/TaskCardComponent.kt
@@ -0,0 +1,173 @@
+package com.loryblu.feature.logbook.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.loryblu.core.ui.theme.LBCardSoftBlue
+import com.loryblu.core.ui.theme.LBSoftBlue
+import com.loryblu.feature.home.R
+
+@Composable
+fun TaskCardComponent(modifier: Modifier = Modifier) {
+    ElevatedCard(
+        modifier
+            .height(208.dp)
+            .width(312.dp)
+    ) {
+        Column(modifier = modifier.fillMaxSize()) {
+            TitleAndDragButton(categoryText = "LoryRotina")
+            TaskContent(modifier = Modifier.height(126.dp))
+            TaskName(modifier = Modifier.height(46.dp), taskName = "Tomar banho")
+        }
+    }
+}
+
+@Composable
+fun TitleAndDragButton(modifier: Modifier = Modifier, categoryText: String) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(LBSoftBlue),
+        contentAlignment = Alignment.Center
+    )
+    {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = categoryText, textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                )
+            ) {
+                Image(painter = painterResource(id = R.drawable.drag_icon), contentDescription = "")
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskContent(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .background(LBCardSoftBlue)
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter =
+            painterResource(id = com.loryblu.data.logbook.R.drawable.bathtime_routine),
+            contentDescription = "",
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(horizontal = 16.dp),
+            contentScale = ContentScale.FillHeight
+        )
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(end = 12.dp)
+                .width(110.dp),
+            horizontalAlignment = Alignment.End,
+        ) {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = LBSoftBlue,
+                    contentColor = Color.Black
+                ),
+                shape = ShapeDefaults.Small,
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Icon(imageVector = Icons.Filled.Edit, contentDescription = "as")
+                Spacer(modifier = Modifier.width(12.dp))
+                Text(
+                    text = "Editar",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                )
+            }
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = LBSoftBlue,
+                    contentColor = Color.Black
+                ),
+                shape = ShapeDefaults.Small,
+                contentPadding = PaddingValues(horizontal = 12.dp, vertical = 0.dp)
+            ) {
+                Icon(imageVector = Icons.Outlined.Delete, contentDescription = "as")
+                Spacer(modifier = Modifier.width(12.dp))
+                Text(
+                    text = "Deletar",
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskName(modifier: Modifier = Modifier, taskName: String) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(LBSoftBlue),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = taskName, textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TaskCardPreview() {
+    TaskCardComponent()
+}
