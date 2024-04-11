3db31eefe3140425634d65eea951b3b4249c4558
leoallvez <leoallvez@hotmail.com>
2024-04-10 22:42:23 -0300

    :sparkles: working edit task summary screen buttons

--- feature/logbook/src/main/java/com/loryblu/feature/logbook/ui/task/edit/LogbookEditTaskViewModel.kt
+++ feature/logbook/src/main/java/com/loryblu/feature/logbook/ui/task/edit/LogbookEditTaskViewModel.kt
@@ -1,12 +1,17 @@
 package com.loryblu.feature.logbook.ui.task.edit
 
 import androidx.lifecycle.ViewModel
+import com.loryblu.data.logbook.local.CategoryItem
 import com.loryblu.feature.logbook.model.LogbookTaskModel
 
 class LogbookEditTaskViewModel(
     private val logbookTaskModel: LogbookTaskModel,
 ) : ViewModel() {
 
-    fun getLogbookTaskModel() : LogbookTaskModel = logbookTaskModel
-
+    fun getLogbookTaskModel() : LogbookTaskModel = LogbookTaskModel(
+        category = CategoryItem.Routine,
+        task = "6dfc15bb-f422-4c75-b2cc-bf3e9806c76a",
+        shift = "morning",
+        frequency = listOf("sun", "mon")
+    )
 }
