44e4b86f6c86aaeb4fd9dd220b26573a2cb8c660
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2024-07-01 09:41:52 -0300

    feat(network): Some refactor at networkModule

--- core/network/src/main/java/com/loryblu/core/network/HttpRoutes.kt
+++ core/network/src/main/java/com/loryblu/core/network/HttpRoutes.kt
@@ -1,10 +1,9 @@
 package com.loryblu.core.network
 
 object HttpRoutes {
-    private const val BASE_URL = BuildConfig.BASE_URL
-    const val REGISTER = "$BASE_URL/auth/register"
-    const val RECOVERY_PASSWORD = "$BASE_URL/auth/recovery"
-    const val NEW_PASSWORD = "$BASE_URL/auth/set-password"
-    const val LOGIN = "$BASE_URL/auth/login"
-    const val TASK = "$BASE_URL/task"
+    const val REGISTER = "/auth/register"
+    const val RECOVERY_PASSWORD = "/auth/recovery"
+    const val NEW_PASSWORD = "/auth/set-password"
+    const val LOGIN = "/auth/login"
+    const val TASK = "/task"
 }
