d37fb4c3560c3d572c5d4e8de02eadad9bf01457
leoallvez <leoallvez@hotmail.com>
2024-08-18 23:53:36 -0300

    feat: open and close menu

--- feature/dashboard/src/main/java/com/odisby/feature/dashboard/ui/DashboardScreen.kt
+++ feature/dashboard/src/main/java/com/odisby/feature/dashboard/ui/DashboardScreen.kt
@@ -1,7 +1,12 @@
 package com.odisby.feature.dashboard.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,37 +18,80 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.loryblu.core.ui.components.LBCardDashboard
 import com.odisby.data.dashboard.local.getAllDashboardItems
 import com.odisby.feature.dashboard.R
 
+@ExperimentalMaterial3Api
 @Composable
 fun DashboardScreen(
     navigateToLogbook: () -> Unit,
     childName: String = "",
 ) {
+    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
 
     val dashboardItems = getAllDashboardItems()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(top = 12.dp, start = 26.dp, bottom = 32.dp),
-            verticalAlignment = CenterVertically,
-            horizontalArrangement = Arrangement.Start
+    Column(modifier = Modifier.fillMaxSize()) {
+        MenuContent(
+            menuIsOpen = openBottomSheet,
+            onClose = { openBottomSheet = false }
+        )
+        AppBar(childName = childName, onMenuClick = { openBottomSheet = true })
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
+            LazyColumn {
+                items(dashboardItems) { item ->
+                    LBCardDashboard(
+                        card = item,
+                        modifier = Modifier
+                            .weight(0.3f)
+                            .fillMaxWidth()
+                            .height(250.dp)
+                            .padding(top = 12.dp, bottom = 12.dp, start = 24.dp, end = 24.dp),
+                        onclick = { navigateToLogbook() }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AppBar(childName: String, onMenuClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 12.dp, start = 26.dp, bottom = 32.dp, end = 26.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Top,
+    ) {
+        Row {
             Image(
                 painter = painterResource(id = R.drawable.logo_home),
                 contentDescription = null,
@@ -57,23 +105,53 @@ fun DashboardScreen(
                 text = "Olá, $childName"
             )
         }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+        MenuIcon(onClick = onMenuClick)
+    }
+}
+
+@Composable
+fun MenuIcon(onClick: () -> Unit) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .size(32.dp)
+            .clip(RoundedCornerShape(20))
+            .border(
+                BorderStroke(1.dp, Color.Gray),
+                shape = RoundedCornerShape(20)
+            )
+            .clickable { onClick.invoke() }
+            .shadow(1.dp, shape = RoundedCornerShape(20))
+            .background(Color.White)
+    ) {
+        Icon(
+            Icons.Filled.Menu,
+            contentDescription = "",
+            tint = Color.Gray,
+            modifier = Modifier.size(22.dp)
+        )
+    }
+
+}
+
+@Preview
+@Composable
+fun AppBarPreview() {
+    AppBar(childName = "Sara") {}
+}
+
+@ExperimentalMaterial3Api
+@Composable
+fun MenuContent(menuIsOpen: Boolean, onClose: () -> Unit) {
+    val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    if (menuIsOpen) {
+        ModalBottomSheet(
+            sheetState = bottomSheetState,
+            onDismissRequest = onClose,
+            modifier = Modifier.fillMaxSize(),
         ) {
-            LazyColumn(
-            ) {
-                items(dashboardItems) { item ->
-                    LBCardDashboard(
-                        card = item,
-                        modifier = Modifier
-                            .weight(0.3f)
-                            .fillMaxWidth()
-                            .height(250.dp)
-                            .padding(top = 12.dp, bottom = 12.dp, start = 24.dp, end = 24.dp),
-                        onclick = { navigateToLogbook() }
-                    )
-                }
+            Box(modifier = Modifier.fillMaxSize()) {
+
             }
         }
     }
