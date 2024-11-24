52d57d31e22f4e4b8a35ebfa8031f418600c8082
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2024-11-24 11:16:23 -0300

    fix(menu): Adding correct paddings to menu sheet

--- feature/dashboard/src/main/java/com/odisby/feature/dashboard/ui/MenuContent.kt
+++ feature/dashboard/src/main/java/com/odisby/feature/dashboard/ui/MenuContent.kt
@@ -10,11 +10,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -86,6 +92,7 @@ fun MenuContent(
         ModalBottomSheet(
             sheetState = state,
             onDismissRequest = onCloseClick,
+            modifier = Modifier.padding(top = 16.dp)
         ) {
             Column(
                 modifier = Modifier
@@ -96,6 +103,7 @@ fun MenuContent(
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
+                        .padding(bottom = 24.dp)
                         .verticalScroll(scrollState),
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
